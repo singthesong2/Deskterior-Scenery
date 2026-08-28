315dc0e3b214d9a402df20e95b35c500c8ce7dd7
WOOWONCHOI <singthesong2@gmail.com>
2026-08-28 12:10:49 +0900

    feat:로그인로그아웃 에러 메세지 처리 및 회원가입 에러메세지 처리, typo:api 경로 오타 수정

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -3,23 +3,15 @@ import { login } from "../api/authApi";
 
 function LoginForm({ setIsLoggedIn, setUserInfo }) {
   const handleLogin = async (id, password) => {
-    try {
-      const result = await login(id, password);
+    const result = await login(id, password);
 
-      console.log("로그인 성공:", result);
+    localStorage.setItem("token", result.token);
+    localStorage.setItem("userInfo", JSON.stringify(result.userInfo));
 
-      localStorage.setItem("token", result.token);
-      localStorage.setItem("userInfo", JSON.stringify(result.userInfo));
+    setIsLoggedIn(true);
+    setUserInfo(result.userInfo);
 
-      setIsLoggedIn(true);
-      setUserInfo(result.userInfo);
-
-      return true;
-    } catch (error) {
-      console.error("로그인 실패:", error.message);
-
-      return false;
-    }
+    return true;
   };
 
   return (
