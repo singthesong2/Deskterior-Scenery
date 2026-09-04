2532d153a71df6f5db98ff01d7811a7525d4fa14
chaege <chaegagim@gmail.com>
2026-09-04 14:11:35 +0900

    chore : 아이템 디테일 이미지 추가 -> detailImages.js 파일에 , 사진 교체 , 사진 밑에 설명 추가

---
+++ src/data/detailImages.js
@@ -0,0 +1,61 @@
+// 상품 상세페이지에서 쓰는 이미지 URL 모음
+
+//Minimal Mushroom Wireless Lamp
+const detailImages = [
+  {
+    id: 1,
+    productId: 101,
+    imageUrl: "https://i.ibb.co/8kLGFxq/Chat-GPT-Image-2026-9-3-04-51-24.webp",
+  },
+  {
+    id: 2,
+    productId: 101,
+    imageUrl: "https://i.ibb.co/Y7wdDt5F/Chat-GPT-Image-2026-9-3-05-02-19.webp",
+  },
+  {
+    id: 3,
+    productId: 101,
+    imageUrl: "https://i.ibb.co/p8RGwB2/Kakao-Talk-20260904-104941287-01.webp",
+  },
+  {
+    id: 4,
+    productId: 101,
+    imageUrl: "https://i.ibb.co/GQVdqzvQ/Kakao-Talk-20260904-104941287-04.webp",
+  },
+
+  //Aluminum Cube Bluetooth Speaker
+  {
+    id: 5,
+    productId: 102,
+    imageUrl: "https://i.ibb.co/0RMVd0P1/Chat-GPT-Image-2026-9-3-04-41-15.webp",
+  },
+  {
+    id: 6,
+    productId: 102,
+    imageUrl: "https://i.ibb.co/pjW2Z1qz/Chat-GPT-Image-2026-9-3-04-42-38.webp",
+  },
+  {
+    id: 7,
+    productId: 102,
+    imageUrl: "https://i.ibb.co/WQTVHZL/Chat-GPT-Image-2026-9-3-04-47-26.webp",
+  },
+
+  //Minimal Desk Pegboard
+  {
+    id: 8,
+    productId: 103,
+    imageUrl: "https://i.ibb.co/MymSbN1Q/Kakao-Talk-20260904-104941287.webp",
+  },
+  {
+    id: 9,
+    productId: 103,
+    imageUrl: "https://i.ibb.co/RTRm265b/Kakao-Talk-20260904-104941287-02.webp",
+  },
+  {
+    id: 10,
+    productId: 103,
+    imageUrl: "https://i.ibb.co/5bMjmFg/Kakao-Talk-20260904-104941287-03.webp",
+  },
+];
+
+export default detailImages;
