37a888d8f7f6ef817f8e70d6c4ef1f760d19d140
WOOWONCHOI <singthesong2@gmail.com>
2026-09-04 10:47:13 +0900

    typo:데이터 api 수정

--- src/data/main.js
+++ src/data/main.js
@@ -2,27 +2,27 @@ const main = [
   {
     id: 1,
     categoryId: "lighting",
-    imageUrl: "<https://i.ibb.co/Qv7Zd4gm/Lighting.webp>",
+    imageUrl: "https://i.ibb.co/Qv7Zd4gm/Lighting.webp",
   },
   {
     id: 2,
     categoryId: "organization",
-    imageUrl: "<https://i.ibb.co/VWwLgVjM/Organization.webp>",
+    imageUrl: "https://i.ibb.co/VWwLgVjM/Organization.webp",
   },
   {
     id: 3,
     categoryId: "digital-electronics",
-    imageUrl: "<https://i.ibb.co/rf7XMB8c/Digital-Electronics.webp>",
+    imageUrl: "https://i.ibb.co/rf7XMB8c/Digital-Electronics.webp",
   },
   {
     id: 4,
     categoryId: "desk-accessories",
-    imageUrl: "<https://i.ibb.co/jKzWGzh/Desk-Accessories.webp>",
+    imageUrl: "https://i.ibb.co/jKzWGzh/Desk-Accessories.webp",
   },
   {
     id: 5,
     categoryId: "objects-stationery",
-    imageUrl: "<https://i.ibb.co/cS6DNGK5/Stationery.webp>",
+    imageUrl: "https://i.ibb.co/cS6DNGK5/Stationery.webp",
   },
 ];
 
