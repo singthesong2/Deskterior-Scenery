e7e6631a8448add5d323a7e1e77e27410799355b
hyunock <ryeohwin9@gmail.com>
2026-09-04 12:29:00 +0900

    feat: add moodkeyword data

--- src/data/main.js
+++ src/data/main.js
@@ -24,6 +24,41 @@ const main = [
     categoryId: "objects-stationery",
     imageUrl: "https://i.ibb.co/cS6DNGK5/Stationery.webp",
   },
+  {
+    id: 6,
+    categoryId: "minimal",
+    imageUrl: "https://i.ibb.co/XZwBHb6M/minimal-2.webp",
+  },
+  {
+    id: 7,
+    categoryId: "natural",
+    imageUrl: "https://i.ibb.co/5WmKLLt1/natural.webp",
+  },
+  {
+    id: 8,
+    categoryId: "hip",
+    imageUrl: "https://i.ibb.co/C3SKrd3p/hip.webp",
+  },
+  {
+    id: 9,
+    categoryId: "metallic",
+    imageUrl: "https://i.ibb.co/G4xmxMTR/metallic.webp",
+  },
+  {
+    id: 10,
+    categoryId: "vintage",
+    imageUrl: "https://i.ibb.co/svLS7jCq/vintage.webp",
+  },
+  {
+    id: 11,
+    categoryId: "cozy",
+    imageUrl: "https://i.ibb.co/prG57hG4/cozy.webp",
+  },
+  {
+    id: 12,
+    categoryId: "pastel",
+    imageUrl: "https://i.ibb.co/qMqDjnKS/pastel.webp",
+  },
 ];
 
 export default main;
