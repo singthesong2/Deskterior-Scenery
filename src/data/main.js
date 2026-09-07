274cc3d7b62b6ba1934595cd134bebefc3f9b480
hyunock <ryeohwin9@gmail.com>
2026-09-07 23:16:58 +0900

    feat: style 관련 데이터에 coordinate 데이터 추가

--- src/data/main.js
+++ src/data/main.js
@@ -1,3 +1,5 @@
+import products from "./products";
+
 const main = [
   {
     id: 1,
@@ -29,49 +31,173 @@ const main = [
     styleId: "minimal",
     name: "Minimal",
     imageUrl: "https://i.ibb.co/XZwBHb6M/minimal-2.webp",
-    products: [3, 39, 31, 16],
-  },
+    coordinate: [
+      {
+        productId: 7, 
+        x: 20.7,
+        y: 16.9,
+      },
+      {productId: 32, 
+        x: 19.1,
+        y: 48.2,
+      },
+      {productId: 16, 
+        x: 64.7,
+        y: 44.8,
+      },
+      {productId: 31, 
+        x: 49.9,
+        y: 70.3,
+      },      
+    ],
+},
   {
     id: 7,
     styleId: "natural",
     name: "Natural",
     imageUrl: "https://i.ibb.co/5WmKLLt1/natural.webp",
-    products: [7, 28, 17],
+    coordinate: [
+      {
+        productId: 7,
+        x: 25.8,
+        y: 17.8,
+      },
+      {
+        productId: 28,
+        x: 30.6,
+        y: 24.2,
+      },
+      {
+        productId: 30,
+        x: 76.6,
+        y: 44.1,
+      },
+      {
+        productId: 17,
+        x: 88.5,
+        y: 70.5,
+      },
+    ]
   },
   {
     id: 8,
     styleId: "hip",
     name: "Hip",
     imageUrl: "https://i.ibb.co/C3SKrd3p/hip.webp",
-    products: [19, 38, 16],
+    coordinate: [
+      {
+        productId: 19,
+        x: 42.2,
+        y: 75.9,
+      },
+      {
+        productId: 38,
+        x: 77.4,
+        y: 40.5,
+      }
+    ],
   },
   {
     id: 9,
     styleId: "metallic",
     name: "Metallic",
     imageUrl: "https://i.ibb.co/G4xmxMTR/metallic.webp",
-    products: [2, 29, 20, 27],
+    coordinate: [
+      {
+        productId: 2,
+        x: 37.2,
+        y: 8.3,
+      },
+      {
+        productId: 29,
+        x: 11.3,
+        y: 63.3,
+      },
+      {
+        productId: 20,
+        x: 77.6,
+        y: 50.2,
+      },
+      {
+        productId: 27,
+        x: 83,
+        y: 82.2,
+      }
+    ]
   },
   {
     id: 10,
     styleId: "vintage",
     name: "Vintage",
     imageUrl: "https://i.ibb.co/svLS7jCq/vintage.webp",
-    products: [26, 14, 13], 
+    coordinate: [
+      {
+        productId: 26,
+        x: 32.3,
+        y: 59.2,
+      },
+      {
+        productId: 14,
+        x: 74.7,
+        y: 16.5,
+      },
+      {
+        productId: 13,
+        x: 80.5,
+        y: 57.9,
+      },
+    ],
   },
   {
     id: 11,
     styleId: "cozy",
     name: "Cozy",
     imageUrl: "https://i.ibb.co/prG57hG4/cozy.webp",
-    products: [9, 37, 22, 35],
+    coordinate: [
+      {
+        productId: 9,
+        x: 24.9,
+        y: 32.2,
+      },
+      {
+        productId: 37,
+        x: 40.4,
+        y: 46.2,
+      },
+      {
+        productId: 22,
+        x: 78.4,
+        y: 39.8,
+      },
+      {
+        productId: 35,
+        x: 90.9,
+        y: 62.8,
+      },
+    ],
   },
   {
     id: 12,
     styleId: "pastel",
     name: "Pastel",
     imageUrl: "https://i.ibb.co/qMqDjnKS/pastel.webp",
-    products: [23, 12, 18],
+    coordinate: [
+      {
+        productId: 23,
+        x: 9,
+        y: 51.6,
+      },
+      {
+        productId: 12,
+        x: 62,
+        y: 65.1,
+      },
+      {
+        productId: 18,
+        x: 19,
+        y: 77.5,
+      },
+    ],
   },
 ];
 
