9b28c3b1ef54a228fe865b51aafff3e5ea148560
hyunock <ryeohwin9@gmail.com>
2026-09-07 13:50:41 +0900

    style: modify HeroSection layout

--- src/styles/MainStyles/HeroSection.styles.jsx
+++ src/styles/MainStyles/HeroSection.styles.jsx
@@ -2,11 +2,14 @@ import styled from "@emotion/styled";
 
 export const HeroContainer = styled.section(({ theme }) => ({
   width: "100%",
-  padding: theme.spacing["3xl"],
-  backgroundColor: theme.colors.cards,
+  height: "720px",
+  padding: theme.spacing["3xl"], // 64px
+  backgroundColor: theme.colors.imagePlaceholder,
 }));
 
-export const HeroTitle = styled.h1(({ theme }) => ({
-  fontSize: theme.fontSize.dpLg,
-  color: theme.colors.textMain,
-}));
+// 히어로 이미지 배너 또는 영상 삽입
+export const HeroMedia = styled.div({
+  width: "100%",
+  height: "100%",
+});
+
