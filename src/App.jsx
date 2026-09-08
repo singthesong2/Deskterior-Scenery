f6311be377d624fcc8105e8db457e23e28cf71aa
chaege <chaegagim@gmail.com>
2026-09-08 16:15:53 +0900

    chore : 상세페이지 분리

--- src/App.jsx
+++ src/App.jsx
@@ -98,11 +98,13 @@ function App() {
       <SignupForm />
 
       <CartPage />
+      {/* /detailpage 라우트로 렌더되므로 겹침 렌더 제거
       <ProductDetailPage
         isLoggedIn={isLoggedIn}
         currentUserId={userInfo?.id ?? null}
         currentUserName={userInfo?.name ?? ""}
       />
+      */}
       <NotFoundPage autoRedirect={false} />
     </>
   );
